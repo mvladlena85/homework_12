4110cf1dc1cc1eae29ad87088725a8cee4643f35
mvladlena85 <mvladlena@yandex.ru>
2022-07-29 18:26:47 +0300

    Added requirements.txt. Added log.txt to .gitignore

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, request, render_template, send_from_directory
 from loader.view import loader_blueprint
 from main.view import main_blueprint
 
-POST_PATH = "posts.json"
-UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
